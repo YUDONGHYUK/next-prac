7690f16aeebba02cf2cd31ec2b4dd09e35dbe71e
doyu <ydh0905@nate.com>
2023-04-04 23:50:22 +0900

    next-13-prac: server component & client component

--- next-13-prac/src/app/page.tsx
+++ next-13-prac/src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
+import Counter from '@/components/Counter';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <h1>Home Page!</h1>;
+  console.log('Hello! - Server');
+
+  return (
+    <>
+      <h1>Home Page!</h1>
+      <Counter />
+    </>
+  );
 }
