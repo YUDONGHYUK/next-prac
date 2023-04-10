888e08c7f4be8fba88e4897a3957f22eadf7de3d
doyu <ydh0905@nate.com>
2023-04-10 23:22:23 +0900

    next-13-prac: redirect & rewrite

--- next-13-prac/next.config.js
+++ next-13-prac/next.config.js
@@ -11,6 +11,37 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/products/deleted_forever',
+        destination: '/products',
+        permanent: true,
+        // Search Engine에 308 status code를 보내 페이지가 영원히 이동했음을 알려준다.
+        // 영원히 변경되었기 Search Engine은 destination을 캐시할 수 있게된다.
+      },
+      {
+        source: '/products/deleted_temp',
+        destination: '/products',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        // 폴더 경로를 살펴보면 /about/me/doyu 페이지로 이동해야 "I am Doyu 😀" 페이지가 나온다.
+        // 하지만 rewrites를 사용하게 되면 /doyu로 이동해도 /about/me/doyu와 동일한 페이지를 볼 수 있게된다.
+        // 물론 원래의 /about/me/doyu로 이동해도 페이지는 나타난다.
+        source: '/doyu',
+        destination: '/about/me/doyu',
+      },
+      {
+        source: '/items/:slug',
+        destination: '/products/:slug',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
