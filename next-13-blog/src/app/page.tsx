1e8d9f412d8ac531eacdc9de2b3bd194cac72dcd
doyu <ydh0905@nate.com>
2023-04-16 16:40:40 +0900

    next-13-blog: Make Home page

--- next-13-blog/src/app/page.tsx
+++ next-13-blog/src/app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from '@/components/Hero';
+import FeaturedPosts from '@/components/FeaturedPosts';
+import CarouselPosts from '@/components/CarouselPosts';
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      {/* @ts-expect-error Async Server Component */}
+      <FeaturedPosts />
+      {/* @ts-expect-error Async Server Component */}
+      <CarouselPosts />
     </>
   );
 }
