7c00431d04583aaf868b3291ad43e18d1ea9689b
doyu <ydh0905@nate.com>
2023-05-11 22:42:02 +0900

    next-13-insta: Add user in sanity database

--- next-13-insta/src/pages/api/auth/[...nextauth].ts
+++ next-13-insta/src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import { addUser } from '@/service/user';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
@@ -9,6 +10,20 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
+    async signIn({ user: { id, name, email, image } }) {
+      if (!email) {
+        // 이메일이 없다면 잘못딘 유저이기 때문에 로그인하지 않음
+        return false;
+      }
+      addUser({
+        id,
+        name: name || '',
+        image,
+        email,
+        username: email.split('@')[0],
+      });
+      return true;
+    },
     async session({ session }) {
       const user = session?.user;
       if (user) {
