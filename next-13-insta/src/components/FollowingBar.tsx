50caed3933c1a95b74e3397bab9431d42ceda52d
doyu <ydh0905@nate.com>
2023-05-16 23:46:25 +0900

    next-13-insta: Add FollowingBar component

--- next-13-insta/src/components/FollowingBar.tsx
+++ next-13-insta/src/components/FollowingBar.tsx
@@ -1,3 +1,44 @@
+'use client';
+import { DetailUser } from '@/model/user';
+import Link from 'next/link';
+import { RotateLoader } from 'react-spinners';
+import useSWR from 'swr';
+import Avatar from './Avatar';
+import ScrollableBar from './ScrollableBar';
+
 export default function FollowingBar() {
-  return <p>Following Bar</p>;
+  const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me');
+  // const users = data?.following;
+  // const users = undefined;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
+
+  return (
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto scrollbar-hide">
+      {loading ? (
+        <RotateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ScrollableBar>
+          {users.map(({ image, username }) => (
+            <Link
+              key={username}
+              className="flex flex-col items-center w-20"
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
+                {username}
+              </p>
+            </Link>
+          ))}
+        </ScrollableBar>
+      )}
+    </section>
+  );
 }
