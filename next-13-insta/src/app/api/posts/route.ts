72063b770249775366fad46edc5bf0cde5443aec
doyu <ydh0905@nate.com>
2023-05-18 23:05:35 +0900

    next-13-insta: Add PostList service logic

--- next-13-insta/src/app/api/posts/route.ts
+++ next-13-insta/src/app/api/posts/route.ts
@@ -1,7 +1,7 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
-import { getUserByUsername } from '@/service/user';
-import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getFollowingPostsOf } from '@/service/posts';
+import { getServerSession } from 'next-auth';
 
 export async function GET() {
   const session = await getServerSession(authOptions);
@@ -11,6 +11,6 @@ export async function GET() {
     return new Response('Authentication Error', { status: 401 });
   }
 
-  return getUserByUsername(user.username) //
+  return getFollowingPostsOf(user.username) //
     .then((data) => NextResponse.json(data));
 }
--- next-13-insta/src/components/PostList.tsx
+++ next-13-insta/src/components/PostList.tsx
@@ -1,3 +1,13 @@
+'use client';
+
+import { SimplePost } from '@/model/post';
+import useSWR from 'swr';
+
 export default function PostList() {
-  return <p>Post List</p>;
+  const { data: posts, isLoading: loading } =
+    useSWR<SimplePost[]>('/api/posts');
+
+  return (
+    <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
+  );
 }
