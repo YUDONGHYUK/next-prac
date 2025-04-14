cb325fe2a5f87593de061cfea2184992c35cf9b9
doyu <ydh0905@nate.com>
2025-04-14 22:31:58 +0900

    next-15-prac: metadata

--- next-15-prac/src/app/(main)/products/[productId]/page.tsx
+++ next-15-prac/src/app/(main)/products/[productId]/page.tsx
@@ -1,8 +1,25 @@
-export default async function ProductDetails({
-  params,
-}: {
+import { Metadata } from 'next';
+
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Galaxy ${id}`);
+    }, 100);
+  });
+
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const productId = (await params).productId;
 
   return <h1>Details about the product {productId}</h1>;
