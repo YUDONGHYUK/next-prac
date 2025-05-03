95a4393b9a1a557162372bf11043b221cd0b748e
doyu <ydh0905@nate.com>
2025-05-03 22:38:06 +0900

    next-15-prac: parallel routes

---
+++ next-15-prac/src/app/(main)/complex-dashboard/components/card.tsx
@@ -0,0 +1,17 @@
+import { CSSProperties, useMemo } from "react";
+
+export function Card({ children }: { children: React.ReactNode }) {
+  const cardStyle: CSSProperties = useMemo(() => {
+    return {
+      padding: "100px",
+      margin: "10px",
+      boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+      border: "1px solid #ddd",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    };
+  }, []);
+
+  return <div style={cardStyle}>{children}</div>;
+}
