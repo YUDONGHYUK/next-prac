f9eb7e9884bc8be52e3d45b3c4acf228def948f1
doyu <ydh0905@nate.com>
2025-05-04 17:21:30 +0900

    next-15-prac: unmatched routes

--- next-15-prac/src/app/(main)/complex-dashboard/@notification/page.tsx
+++ next-15-prac/src/app/(main)/complex-dashboard/@notification/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
 import { Card } from "../components/card";
 
 export default function Notification() {
-  return <Card>Notifications</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <div>
+        <Link href={"/complex-dashboard/archived"}>Archived</Link>
+      </div>
+    </Card>
+  );
 }
